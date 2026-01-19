4325deb0b30daf1dc1ca6141b3284a9da791f05a
Yu-En Wang <ernestwang100@gmail.com>
2026-01-18 19:28:14 -0500

    Add Trip Service module structure

---
+++ backend/trip-service/src/main/java/com/ridesharing/trip/TripServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ridesharing.trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripServiceApplication.class, args);
+    }
+}
